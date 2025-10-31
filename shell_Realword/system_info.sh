7b633362d0a2297d53f42c7db1d6982175300c87
Gundapu <lakshmanx.gundapu@intel.com>
2025-10-31 07:35:03 +0530

    shell Practice

--- shell_Realword/system_info.sh
+++ shell_Realword/system_info.sh
@@ -4,4 +4,9 @@
 
 echo "Hostname: $(hostname)"
 echo "OS: $(cat /etc/os-release | head -1 | cut -d '"' -f2)"
-echo "OS: $(cat /etc/os-release | awk -F '"' 'NR==1 {print $2}')"
+#echo "OS: $(cat /etc/os-release | awk -F '"' 'NR==1 {print $2}')"
+#above one is with awk 
+
+echo "kernel: $(uname -r)"
+
+echo "CPU Info: $(cat /proc/cpuinfo | awk -F ':' 'NR==5 {print $2}')"
