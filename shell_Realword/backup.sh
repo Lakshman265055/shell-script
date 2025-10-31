447e2b847b912f14674d2fd6b1b472623f624a3b
Gundapu <lakshmanx.gundapu@intel.com>
2025-10-31 13:18:32 +0530

    shell Practice

--- shell_Realword/backup.sh
+++ shell_Realword/backup.sh
@@ -3,10 +3,12 @@
 if [[ -z "$1" ]]; then
     echo "script usage: ${0} <folder/path>"
     exit 1
+fi
 
 if [[ ! -d "$1" ]]; then
     echo " folder is not preset please check thr path"
     exit 1
+fi
 
 SOURCE_DIR=$1
 
@@ -14,6 +16,6 @@ BACKUP_DIR="/tmp/backups"
 
 mkdir -p $BACKUP_DIR
 
-BACkUP_FILE="${BACKUP_DIR}/$(basename "$BACKUP_DIR")_backup_$(date +%F_%H-%M-%S).tar.gz"
+BACKUP_FILE="${BACKUP_DIR}/$(basename "$BACKUP_DIR")_backup_$(date +%F_%H-%M-%S).tar.gz"
 
 tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
