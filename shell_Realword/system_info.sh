0f5735e9ef29a98e80bb4f6417adead13d363ab2
Gundapu <lakshmanx.gundapu@intel.com>
2025-10-31 06:59:34 +0530

    shell Practice

--- shell_Realword/system_info.sh
+++ shell_Realword/system_info.sh
@@ -3,3 +3,5 @@
 # this script is to check the system_info
 
 echo "Hostname: $(hostname)"
+echo "OS: $(cat /etc/os-release | head -1 | cut -d '"' -f2)"
+echo "OS: $(cat /etc/os-release | awk -F '"' 'NR==1 {print $2})
